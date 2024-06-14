86325d5089aec8dae583bff316e9d3fc81d1e55d
codeprozoro <solocailin@gmail.com>
2024-06-14 19:40:51 +0500

    first commit

---
+++ OnlyNeed.js
@@ -0,0 +1,68 @@
+// Primitive data types
+
+// Primitive data types in programming languages are the most basic data types available and are built into the language. These data types are used to represent simple values.
+
+// Here are some common primitive data types:
+
+// Integer: A whole number without a decimal point (e.g. 10, -5, 0).
+
+// Float: A number with a decimal point (e.g. 3.14, -2.5, 0.0).
+
+// Boolean: A data type that can only take two values: true or false.
+
+// Character: A single character (e.g. 'a', 'Z', '@').
+
+// String: A sequence of characters (e.g. "Hello, World!", "12345").
+
+// Null: A special value that represents the absence of a value.
+
+// Different programming languages may have different primitive data types, but the ones listed above are very common in most languages.
+
+
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// // Reference 
+// // reference = [], (), {}
+// // Examples
+// let j = [12, 32, 3232, 3232, 333]
+
+// let obj = {
+//     name: "Makima",
+//     age: 69
+// }
+// console.log(obj.age);
+
+// let age = 3
+// function value() {
+//     if (age < 14) {
+//         console.log("You can't enter");
+//         return true
+
+//     }
+//     else {
+//         console.log("You can enter");
+//         return false
+//     }
+
+// }
+// value()
+
+// var obj1 = {
+//     name1: "John",
+//     age1: 30
+// };
+// console.log(obj1.name1)
+
+// function num(value){
+//     console.log(value);
+
+
+
+// }
+// num(34)
+
+let arr = [343, 4343, 434, 34]
+arr.push(3)
